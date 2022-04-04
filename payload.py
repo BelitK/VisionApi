de9f91d95629adf03550700eeb4cb49e09f4ca9d
BelitK <belitberdelk@gmail.com>
2022-04-05 01:09:42 +0300

    added boxes for presentation

--- payload.py
+++ payload.py
@@ -1,10 +1,11 @@
 import base64
-import numpy as np
+#import numpy as np
 import cv2
 import requests
 import json
+import sys
 from IPython import embed
-test = str(input("image name"))
+test = sys.argv[1]
 thresh = float(input("threshold (0-1)"))
 with open(test, "rb") as f:
     im_b64 = base64.b64encode(f.read()).decode("utf-8")
@@ -19,3 +20,20 @@ pay = {"b64":im_b64,"threshold":thresh}
 #print(pay2)
 a=requests.get(url,json=json.dumps(pay))
 print(a.content)
+
+
+
+img = cv2.imread(f"{test}")
+
+data = json.loads(a.content)
+for box in data["boxes"]:
+    
+    x, y, w, h = box
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,0), 1)
+    embed()
+cv2.imshow("Image", img)
+cv2.waitKey(0) # waits until a key is pressed
+cv2.destroyAllWindows() # destroys the window showing image
+#key = cv2.waitKey(0)
+
+
