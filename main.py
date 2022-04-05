1790a87d64fe2f7b2fbdf0d10bd52bd5e037bf6c
BelitK <belitberdelk@gmail.com>
2022-04-05 22:43:08 +0300

    added human detection and linted

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI, Request
 import json
-from funcs import get_coor
+from funcs import get_coor, get_human
+from fastapi import FastAPI, Request
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
-        return{"Made By":"BelitK"}
+    return{"Made By": "BelitK"}
+
+
 @app.get("/vision")
 async def boxes_and_confidence(request: Request):
-        
-        a= await request.json()
-        image = json.loads(a)["b64"]
-        threshold = json.loads(a)["threshold"]
-        return get_coor(image,threshold)
+    """Object detection using YOLO"""
+    a = await request.json()
+    image = json.loads(a)["b64"]
+    threshold = json.loads(a)["threshold"]
+    return get_coor(image, threshold)
+
+
+@app.get("/human")
+async def human_detection(request: Request):
+    """Human detection using Hog-SVM"""
+    a = await request.json()
+    image = json.loads(a)["b64"]
+    return get_human(image)
--- payload.py
+++ payload.py
@@ -3,15 +3,24 @@ import cv2
 import requests
 import json
 import sys
-from IPython import embed
 test = sys.argv[1]
-thresh = float(input("threshold (0-1)"))
+ob_type = sys.argv[2]
+
+thresh = float(input("threshold (0-1) : "))
 with open(test, "rb") as f:
     im_b64 = base64.b64encode(f.read()).decode("utf-8")
 
-url = "http://ec2-3-144-225-203.us-east-2.compute.amazonaws.com:8000/vision"
-pay = {"b64":im_b64,"threshold":thresh}
-a=requests.get(url,json=json.dumps(pay))
+if ob_type == 'human':
+    pay = {"b64": im_b64}
+    url = "http://ec2-3-144-225-203.us-east-2.compute.amazonaws.com:8000/human"
+
+else:
+    pay = {"b64": im_b64,
+           "threshold": thresh}
+    url = "http://ec2-3-144-225-203.us-east-2.compute.amazonaws.com:8000/vision"
+
+
+a = requests.get(url, json=json.dumps(pay))
 print(a.content)
 
 
@@ -20,12 +29,9 @@ img = cv2.imread(f"{test}")
 
 data = json.loads(a.content)
 for box in data["boxes"]:
-    
+
     x, y, w, h = box
-    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,0), 1)
-    #embed()
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 1)
 cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
